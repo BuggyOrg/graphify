15b0551a388a5f714c30784d4538c41308167f05
Maximilian Klein <dasmaxl@gmail.com>
2016-04-27 14:07:34 +0200

    (feature) Accept piped inputs for CLI.

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 var Nightmare = require('nightmare')
 var fs = require('fs')
 var vo = require('vo')
+var getStdin = require('get-stdin')
 
 // cli arguments and defaults
 var arg0 = process.argv[2] || 'examples/testgraph.json'
@@ -11,41 +12,50 @@ var path = require('path')
 var graphifyPath = path.normalize(path.join(__dirname, '../'))
 
 /* read input graph */
-var inputPath = path.join(graphifyPath, arg0)
-var input = fs.readFileSync(inputPath, 'utf8')
+var inputPromise = null
+if (arg0) {
+  var inputPath = path.join(graphifyPath, arg0)
+  inputPromise = Promise.resolve(fs.readFileSync(inputPath, 'utf8'))
+} else {
+  inputPromise = getStdin()
+}
 
-/* read style sheet */
-var cssPath = path.join(graphifyPath, arg1)
-var css = fs.readFileSync(cssPath, 'utf8')
+inputPromise.then((input) => {
+  
+  /* read style sheet */
+  var cssPath = path.join(graphifyPath, arg1)
+  var css = fs.readFileSync(cssPath, 'utf8')
 
-vo(function* () {
+  vo(function* () {
 
-  /* Nightmare Options */
-  var nightmare = Nightmare(
+    /* Nightmare Options */
+    var nightmare = Nightmare(
+    {
+      plugins: true,
+      allowDisplayingInsecureContent: true,
+      allowRunningInsecureContent: true
+    })
+
+    /* Open page in nightmare and read svg result */
+    var result = yield nightmare
+      .goto(path.join('file://', graphifyPath, 'app/index.html'))
+      .type('#txtInput', input)
+      .click('#btnInput')
+      .wait('#svgOutput')
+      .wait(1000)
+      .evaluate(function(css) {
+        var svg = document.getElementById('svgOutput')
+        svg.innerHTML = "<style> /* <![CDATA[ */" + css + "/* ]]> */ </style>" + svg.innerHTML
+        return svg.outerHTML
+      }, css)
+
+    console.log(result)
+
+    yield nightmare.end()
+
+  })(function (err, result)
   {
-    plugins: true,
-    allowDisplayingInsecureContent: true,
-    allowRunningInsecureContent: true
-  })
+    if (err) return console.log(err);
+  });
 
-  /* Open page in nightmare and read svg result */
-  var result = yield nightmare
-    .goto(path.join('file://', graphifyPath, 'app/index.html'))
-    .type('#txtInput', input)
-    .click('#btnInput')
-    .wait('#svgOutput')
-    .wait(1000)
-    .evaluate(function(css) {
-      var svg = document.getElementById('svgOutput')
-      svg.innerHTML = "<style> /* <![CDATA[ */" + css + "/* ]]> */ </style>" + svg.innerHTML
-      return svg.outerHTML
-    }, css)
-
-  console.log(result)
-
-  yield nightmare.end()
-
-})(function (err, result)
-{
-  if (err) return console.log(err);
-});
+})
