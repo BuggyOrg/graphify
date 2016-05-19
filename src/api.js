6a2fc7c621a2081b72452762df9966e1ce575533
Maik Marschner <an@maik.tk>
2016-05-19 12:24:35 +0200

    (fix) Fix the api and use it for the cli.

--- src/api.js
+++ src/api.js
@@ -1,40 +1,38 @@
 var Nightmare = require('nightmare')
-var fs = require('fs')
-var vo = require('vo')
 var path = require('path')
 
-module.exports = (input, cssFile = 'src/style.css') => {
-  var graphifyPath = path.normalize(path.join(__dirname, '../'))
-  var cssPath = path.join(graphifyPath, cssFile)
-  var css = fs.readFileSync(cssPath, 'utf8')
+var graphifyPath = path.normalize(path.join(__dirname, '../'))
 
-  return new Promise((resolve, reject) => {
-    vo(function * () {
-      /* Nightmare Options */
-      let nightmare = Nightmare({
-        plugins: true,
-        allowDisplayingInsecureContent: true,
-        allowRunningInsecureContent: true
-      })
-
-      /* Open page in nightmare and read svg result */
-      let result = yield nightmare
-        .goto(path.join('file://', graphifyPath, 'app/index.html'))
-        .type('#txtInput', input)
-        .click('#btnInput')
-        .wait('#svgOutput')
-        .wait(1000)
-        .evaluate(function (css) {
-          var svg = document.getElementById('svgOutput')
-          svg.innerHTML = '<style> /* <![CDATA[ */' + css + '/* ]]> */ </style>' + svg.innerHTML
-          return svg.outerHTML
-        }, css)
-      yield nightmare.end()
-      resolve(result)
-    })((err, result) => {
-      if (err) {
-        reject(err)
-      }
-    })
+module.exports = (input) => {
+  /* Nightmare Options */
+  var nightmare = Nightmare({
+    plugins: true,
+    allowDisplayingInsecureContent: true,
+    allowRunningInsecureContent: true
   })
+  /* Open page in nightmare and read svg result */
+
+  return Promise.resolve(nightmare
+    .goto(path.join('file://', graphifyPath, 'app/index.html'))
+    .type('#txtInput', input)
+    .click('#btnInput')
+    .wait('#svgOutput')
+    .evaluate(function () {
+      var svg = document.getElementById('svgOutput')
+      svg.removeAttribute('baseprofile')
+      svg.removeAttribute('id')
+      Array.prototype.forEach.call(svg.querySelectorAll('*'), (e) => {
+        e.removeAttribute('data-meta')
+        e.removeAttribute('class')
+      })
+      return svg.outerHTML
+    })
+    .end())
+    .then((svg) => {
+      var result =
+        '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
+        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' +
+        svg
+      return result
+    })
 }
--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 
 import program from 'commander'
-import Nightmare from 'nightmare'
 import fs from 'fs'
-import vo from 'vo'
 import getStdin from 'get-stdin'
 import path from 'path'
+import generateSvg from './api'
 
 program
   .version(JSON.parse(fs.readFileSync(path.join(__dirname, '/../package.json')))['version'])
   .option('-f, --graphfile <graph file>', 'Set graph file to convert. If none is given, stdin is read.')
   .option('-o, --out <svg output file>', 'Set a custom output path. If none is given, stdout is used.')
-  .option('--style <css file>', 'Use a custom stylesheet.')
   .parse(process.argv)
-
-// read home folder
-const graphifyPath = path.normalize(path.join(__dirname, '../'))
 
 /* read input graph */
 let inputPromise
@@ -25,52 +20,15 @@ if (program.graphfile) {
   inputPromise = getStdin()
 }
 
-inputPromise.then((input) => {
-  /* read style sheet */
-  var cssPath = program.style || path.join(graphifyPath, 'src/style.css')
-  var css = fs.readFileSync(cssPath, 'utf8')
-
-  vo(function * () {
-    /* Nightmare Options */
-    var nightmare = Nightmare(
-      {
-        plugins: true,
-        allowDisplayingInsecureContent: true,
-        allowRunningInsecureContent: true
-      })
-
-    /* Open page in nightmare and read svg result */
-    var result = yield nightmare
-      .goto(path.join('file://', graphifyPath, 'app/index.html'))
-      .type('#txtInput', input)
-      .click('#btnInput')
-      .wait('#svgOutput')
-      .wait(1000)
-      .evaluate(function (css) {
-        var svg = document.getElementById('svgOutput')
-        svg.removeAttribute('baseprofile')
-        svg.removeAttribute('id')
-        Array.prototype.forEach.call(svg.querySelectorAll('*'), (e) => {
-          e.removeAttribute('data-meta')
-          e.removeAttribute('class')
-        })
-        return svg.outerHTML
-      }, css)
-
-    result = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
-             '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' +
-             result
-
+inputPromise
+  .then(generateSvg)
+  .then((svg) => {
     if (program.out) {
-      fs.writeFileSync(program.out, result, 'utf8')
+      fs.writeFileSync(program.out, svg + '\n', 'utf8')
     } else {
-      process.stdout.write(result)
-    }
-
-    yield nightmare.end()
-  })(function (err, result) {
-    if (err) {
-      console.error(err)
+      process.stdout.write(svg + '\n')
     }
   })
-})
+  .catch((err) => {
+    console.error('Generating the SVG failed', err)
+  })
