3f0c587ca53f27a2e804c7d2ac5cf2a6bb4e395d
leMaik <an@maik.tk>
2017-09-14 16:26:48 +0200

    (cleanup) Remove old layout code.

--- src/measure.js
+++
@@ -1,8 +0,0 @@
-const calculateSize = require('calculate-size')
-window.measureText = (text, style) => {
-  if (Array.isArray(text)) {
-    return text.map((t) => calculateSize.default(t, style))
-  } else {
-    return calculateSize.default(text, style)
-  }
-}
